2499b683a43b7b2c50b933aa6ee648e857657e2b
titu84 <krzysztof.sauermann@gmail.com>
2018-10-11 09:26:36 +0200

    test

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -17,6 +17,7 @@ namespace Exam
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ParentForm());
+
             //bla bla
         }
     }
